3a8b4ff5236eedf30161f05eb9ab0c5cfb813d87
badre khaldi <39647220+didroo@users.noreply.github.com>
2022-04-16 02:32:46 +0000

    corection des erreurs

---
+++ oc-install-ide-cours-master/src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.ideinstall;
+
+public class BasicExample {
+
+	public static void main(String[] args) {
+		 int index;
+		for(int i = 0; i>=3; i++) {
+			System.out.println("Hello bugs !");
+		}
+	}
+}
