82193273b301c7f016d6320903fc6e689ba42428
badre khaldi <39647220+didroo@users.noreply.github.com>
2022-04-16 04:49:02 +0000

    ajouter message badre

--- oc-install-ide-cours-master/src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ oc-install-ide-cours-master/src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		 int index;
 		for(int i = 0; i>=3; i++) {
-			System.out.println("Hello bugs !");
+			System.out.println("Hello badre !");
 		}
 	}
 }
